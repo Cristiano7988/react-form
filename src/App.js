f8be650a1b4d7c35645bdf86c28c6921f3cdbd71
cristiano7988 <cristianooliveiramorales@gmail.com>
2021-03-27 02:14:13 -0300

    Formulário de cadastro com Function Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import { Container, Typography } from '@material-ui/core';
+import 'fontsource-roboto';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <Container component="article" maxWidth="sm">
+        <Typography variant="h3" component="h1" align="center">Formulário de Cadastro</Typography>
+        <FormularioCadastro aoEnviar={aoEnviarForm} validarCpf={validarCpf} />
+      </Container>
+    );
+  }
+}
+
+function aoEnviarForm(dados) {
+  console.log(dados)
+}
+
+function validarCpf(cpf) {
+  if(cpf.length !== 11) {
+    return {valido:false, texto: "CPF deve ter 11 digitos"}
+  } else {
+    return {valido: true, texto: ""}
+  }
 }
 
 export default App;
