d18247595f0ebda2b7260e1e99ba5eca6c69b1e0
cristiano7988 <cristianooliveiramorales@gmail.com>
2021-03-29 12:12:30 -0300

    Tratamento para default

--- src/contexts/ValidacoesCadastro.js
+++ src/contexts/ValidacoesCadastro.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const ValidacoesCadastro = React.createContext();
+const ValidacoesCadastro = React.createContext({
+    cpf: semValidacao,
+    senha: semValidacao,
+    nome: semValidacao
+});
+
+function semValidacao() {
+    return {valido: true, texto: ""}
+}
 
 export default ValidacoesCadastro;
